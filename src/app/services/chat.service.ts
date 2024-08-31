327f32346953f9beab97c27c4f50683ed1710255
Baptiste FIORINA <baptistefiorina@gmail.com>
2024-08-31 15:45:21 +0200

    read message

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -69,70 +69,70 @@ export class ChatService {
   
   constructor() {
     this.userSubscription = this.user$.subscribe((aUser: User | null) => {
-        this.currentUser = aUser;
+      this.currentUser = aUser;
     });
   }
 
   // Login Friendly Chat.
   login() {
     signInWithPopup(this.auth, this.provider).then((result) => {
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        this.router.navigate(['/', 'chat']);
-        return credential;
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      this.router.navigate(['/', 'chat']);
+      return credential;
     })
   }
 
   // Logout of Friendly Chat.
   logout() {
     signOut(this.auth).then(() => {
-        this.router.navigate(['/', 'login'])
-        console.log('signed out');
+      this.router.navigate(['/', 'login'])
+      console.log('signed out');
     }).catch((error) => {
-        console.log('sign out error: ' + error);
+      console.log('sign out error: ' + error);
     })
   }
 
   // Adds a text or image message to Cloud Firestore.
-addMessage = async (
-  textMessage: string | null,
-  imageUrl: string | null,
-): Promise<void | DocumentReference<DocumentData>> => {
+  addMessage = async (
+    textMessage: string | null,
+    imageUrl: string | null,
+    ): Promise<void | DocumentReference<DocumentData>> => {
   // ignore empty messages
-  if (!textMessage && !imageUrl) {
-    console.log(
-      "addMessage was called without a message",
-      textMessage,
-      imageUrl,
-    );
-    return;
-  }
+    if (!textMessage && !imageUrl) {
+      console.log(
+        "addMessage was called without a message",
+        textMessage,
+        imageUrl,
+        );
+      return;
+    }
 
-  if (this.currentUser === null) {
-    console.log("addMessage requires a signed-in user");
-    return;
-  }
+    if (this.currentUser === null) {
+      console.log("addMessage requires a signed-in user");
+      return;
+    }
 
-  const message: ChatMessage = {
-    name: this.currentUser.displayName,
-    profilePicUrl: this.currentUser.photoURL,
-    timestamp: serverTimestamp(),
-    uid: this.currentUser?.uid,
+    const message: ChatMessage = {
+      name: this.currentUser.displayName,
+      profilePicUrl: this.currentUser.photoURL,
+      timestamp: serverTimestamp(),
+      uid: this.currentUser?.uid,
+    };
+
+    textMessage && (message.text = textMessage);
+    imageUrl && (message.imageUrl = imageUrl);
+
+    try {
+      const newMessageRef = await addDoc(
+        collection(this.firestore, "messages"),
+        message,
+        );
+      return newMessageRef;
+    } catch (error) {
+      console.error("Error writing new message to Firebase Database", error);
+      return;
+    }
   };
-
-  textMessage && (message.text = textMessage);
-  imageUrl && (message.imageUrl = imageUrl);
-
-  try {
-    const newMessageRef = await addDoc(
-      collection(this.firestore, "messages"),
-      message,
-    );
-    return newMessageRef;
-  } catch (error) {
-    console.error("Error writing new message to Firebase Database", error);
-    return;
-  }
-};
 
 
   // Saves a new message to Cloud Firestore.
@@ -140,10 +140,13 @@ addMessage = async (
     return this.addMessage(messageText, null);
   };
 
-  // Loads chat messages history and listens for upcoming ones.
+  // Loads chat message history and listens for upcoming ones.
   loadMessages = () => {
-    return null as unknown;
-  };
+  // Create the query to load the last 12 messages and listen for new ones.
+    const recentMessagesQuery = query(collection(this.firestore, 'messages'), orderBy('timestamp', 'desc'), limit(12));
+  // Start listening to the query.
+    return collectionData(recentMessagesQuery);
+  }
 
   // Saves a new message containing an image in Firebase.
   // This first saves the image in Firebase storage.
@@ -161,7 +164,7 @@ addMessage = async (
     path: string,
     input: HTMLInputElement,
     contentType: any
-  ) {
+    ) {
     return null;
   }
   // Requests permissions to show notifications.
