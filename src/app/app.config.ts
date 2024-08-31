da8ad705468541d9d69cd08576a22f12187d35ae
Baptiste FIORINA <baptistefiorina@gmail.com>
2024-08-31 14:08:37 +0200

    fix build

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
@@ -11,14 +11,12 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-81412","appId":"1:612242866770:web:71b51163ca8c11535f558b","storageBucket":"friendlychat-81412.appspot.com","locationId":"europe-west","apiKey":"AIzaSyCdFUF-TjNO9vXQuqWyf8mOob-2g337f7U","authDomain":"friendlychat-81412.firebaseapp.com","messagingSenderId":"612242866770"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+    provideFunctions(() => getFunctions()),
+    provideStorage(() => getStorage()),
+    provideMessaging(() => getMessaging()),
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-81412","appId":"1:612242866770:web:71b51163ca8c11535f558b","storageBucket":"friendlychat-81412.appspot.com","apiKey":"AIzaSyCdFUF-TjNO9vXQuqWyf8mOob-2g337f7U","authDomain":"friendlychat-81412.firebaseapp.com","messagingSenderId":"612242866770"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
